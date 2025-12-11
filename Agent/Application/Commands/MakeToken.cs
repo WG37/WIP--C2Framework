26b5cdc6dab9eba8010237963fc7b695cf8002d0
WG37 <willg2070@gmail.com>
2025-12-11 13:05:37 +1100

    feat(commands): add make-token command

--- Agent/Application/Commands/MakeToken.cs
+++ Agent/Application/Commands/MakeToken.cs
@@ -1,5 +1,7 @@
 ﻿using AgentClient.Domain.Models.Agents;
 using AgentClient.Infrastructure.Native;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 
 namespace AgentClient.Application.Commands
 {
@@ -9,25 +11,48 @@ namespace AgentClient.Application.Commands
 
         public override string Execute(AgentTask task)
         {
-            
-
             var userDomain = task.Arguments[0];
             var password = task.Arguments[1];
 
-            var split = userDomain.Split("\\");
+            var split = userDomain.Split('\\');
             var domain = split[0];
             var username = split[1];
 
-            IntPtr hToken;
-            
-            if (Advapi32.LogonUserA(username, domain, password,
-                            Advapi32.LogonUserType.LOGON32_LOGON_NEW_CREDENTIALS,
-                            Advapi32.LogonUserProvider.LOGON32_PROVIDER_DEFAULT, out hToken))
-            {
-                // impersonate token
-            }
+            IntPtr hToken = IntPtr.Zero;
 
-            return "Failed to create token";
+            try
+            {
+                if (!Advapi32.LogonUserA(username, domain, password,
+                            Advapi32.LogonUserType.LOGON32_LOGON_INTERACTIVE,
+                            Advapi32.LogonUserProvider.LOGON32_PROVIDER_DEFAULT,
+                            out hToken))
+                {
+                    int error = Marshal.GetLastWin32Error();
+
+                    hToken = IntPtr.Zero;
+                    return $"Failed to create token. Win32 Error: {error}";
+                }
+
+                if (!Advapi32.ImpersonateLoggedOnUser(hToken))
+                {
+                    return "Successful token creation, but failed to impersonate";
+                }
+
+                // TODO: Privileged tasks/actions 
+                using (var identity = new WindowsIdentity(hToken))
+                {
+                    return $"Successful token impersonation {identity.Name}";
+                }
+            }
+            finally
+            {
+                Advapi32.RevertToSelf();
+
+                if (hToken != IntPtr.Zero)
+                {
+                    Kernel32.CloseHandle(hToken);
+                }
+            }
         }
     }
 }
